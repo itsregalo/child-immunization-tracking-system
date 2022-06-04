23dbc1745921c854f5d3f300da98a797f00c02ad
retech <itsregalo047@gmail.com>
2022-06-04 11:21:40 +0300

    admin dashboard

--- customadmin/urls.py
+++ customadmin/urls.py
@@ -6,5 +6,22 @@ app_name = 'custom-admin'
 
 urlpatterns = [
     path('', IndexView, name='index'),
-    path('doctors/', DoctorList, name='doctors')
+    path('doctors/', DoctorList, name='doctors'),
+    path('doctors/<int:pk>/', DoctorDetail, name='doctor-detail'),
+    path('doctors/<int:pk>/delete/', DoctorDelete, name='doctor-delete'),
+    path('doctors/create/', DoctorCreate, name='doctor-create'),
+    path('parents/', ParentList, name='parents'),
+    path('parents/<int:pk>/', ParentDetail, name='parent-detail'),
+    path('parents/<int:pk>/delete/', ParentDelete, name='parent-delete'),
+    path('parents/create/', ParentCreate, name='parent-create'),
+    path('children/', ChildList, name='children'),
+    path('children/<int:pk>/', ChildDetail, name='child-detail'),
+    path('children/<int:pk>/delete/', ChildDelete, name='child-delete'),
+    path('children/create/', ChildCreate, name='child-create'),
+    path('vaccines/', VaccinesList, name='vaccines'),
+    path('vaccines/<int:pk>/', VaccinesDetail, name='vaccines-detail'),
+    path('vaccines/<int:pk>/delete/', VaccinesDelete, name='vaccines-delete'),
+    path('vaccines/create/', VaccinesCreate, name='vaccines-create'),
+
+    
 ]
--- customadmin/views.py
+++ customadmin/views.py
@@ -14,6 +14,24 @@ def DoctorList(request, *args, **kwargs):
     }
     return render(request, 'admin-dash/doctors.html', context)
 
+def DoctorDetail(request, pk):
+    doctor = Doctor.objects.get(pk=pk)
+
+    context = {
+        'doctor':doctor
+    }
+    return render(request, 'admin-dash/doctor-detail.html', context)
+
+def DoctorDelete(request, pk):
+    doctor = Doctor.objects.get(pk=pk)
+    doctor.delete()
+
+    return render(request, 'admin-dash/index.html')
+
+def DoctorCreate(request, *args, **kwargs):
+    return render(request, 'admin-dash/doctor-create.html')
+
+
 def ParentList(request, *args, **kwargs):
     parents = Parent.objects.all()
 
@@ -21,6 +39,23 @@ def ParentList(request, *args, **kwargs):
         'parents':parents
     }
     return render(request, 'admin-dash/parents.html', context)
+
+def ParentDetail(request, pk):
+    parent = Parent.objects.get(pk=pk)
+
+    context = {
+        'parent':parent
+    }
+    return render(request, 'admin-dash/parent-detail.html', context)
+
+def ParentDelete(request, pk):
+    parent = Parent.objects.get(pk=pk)
+    parent.delete()
+
+    return render(request, 'admin-dash/index.html')
+
+def ParentCreate(request, *args, **kwargs):
+    return render(request, 'admin-dash/parent-create.html')
 
 def ChildList(request, *args, **kwargs):
     children = Child.objects.all()
@@ -30,6 +65,23 @@ def ChildList(request, *args, **kwargs):
     }
     return render(request, 'admin-dash/children.html', context)
 
+def ChildDetail(request, pk):
+    child = Child.objects.get(pk=pk)
+
+    context = {
+        'child':child
+    }
+    return render(request, 'admin-dash/child-detail.html', context)
+
+def ChildDelete(request, pk):
+    child = Child.objects.get(pk=pk)
+    child.delete()
+
+    return render(request, 'admin-dash/index.html')
+
+def ChildCreate(request, *args, **kwargs):
+    return render(request, 'admin-dash/child-create.html')
+
 def VaccinesList(request, *args, **kwargs):
     vaccines = Vaccines.objects.all()
 
@@ -37,3 +89,20 @@ def VaccinesList(request, *args, **kwargs):
         'vaccines':vaccines
     }
     return render(request, 'admin-dash/vaccines.html', context)
+
+def VaccinesDetail(request, pk):
+    vaccine = Vaccines.objects.get(pk=pk)
+
+    context = {
+        'vaccine':vaccine
+    }
+    return render(request, 'admin-dash/vaccines-detail.html', context)
+
+def VaccinesDelete(request, pk):
+    vaccine = Vaccines.objects.get(pk=pk)
+    vaccine.delete()
+
+    return render(request, 'admin-dash/index.html')
+
+def VaccinesCreate(request, *args, **kwargs):
+    return render(request, 'admin-dash/vaccines-create.html')
