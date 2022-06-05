d419eedb8891fbe210ed377ce6fd649345c241d9
retech <itsregalo047@gmail.com>
2022-06-05 08:15:05 +0300

    api key hidden

--- txt.py
+++ txt.py
@@ -1,10 +1,11 @@
 
 # import package
+from decouple import config
 import africastalking
 
 # Initialize SDK
 username = "vax"    # use 'sandbox' for development in the test environment
-api_key = "627ec15b8423b66494193f75466c00997afb091cfe06e4a081a1dc95b151f544"      # use your sandbox app API key for development in the test environment
+api_key = config('API_KEY')      # use your sandbox app API key for development in the test environment
 africastalking.initialize(username, api_key)
 
 # Initialize a service e.g. SMS
